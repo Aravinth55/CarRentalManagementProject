127b1a889610bae80f1a0b54ddfc541279d408ca
Aravinth55 <aravinth.venkatesan@chainsys.com>
2022-08-16 18:12:04 +0530

    sonarcolud cleard

--- src/main/java/com/chainsys/carrental/model/CarRental.java
+++ src/main/java/com/chainsys/carrental/model/CarRental.java
@@ -11,12 +11,9 @@ import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
--- src/main/java/com/chainsys/carrental/service/CarRentalService.java
+++ src/main/java/com/chainsys/carrental/service/CarRentalService.java
@@ -1,6 +1,5 @@
 package com.chainsys.carrental.service;
 
-import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
